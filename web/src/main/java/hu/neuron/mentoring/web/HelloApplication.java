d6045038bb8e60595e6bdae1ecc7e7651f3f72b4
PappDávid <PappDavid32@gmail.com>
2023-10-31 20:05:38 +0100

    Created project base

---
+++ web/src/main/java/hu/neuron/mentoring/web/HelloApplication.java
@@ -0,0 +1,9 @@
+package hu.neuron.mentoring.web;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
