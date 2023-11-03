655bb9046a0906d4c10f3c75650e3d484b12aaf2
PappDávid <PappDavid32@gmail.com>
2023-11-03 16:55:46 +0100

    Added webFilter to login authentication

--- web/src/main/java/hu/neuron/mentoring/web/LoginServlet.java
+++ web/src/main/java/hu/neuron/mentoring/web/LoginServlet.java
@@ -23,33 +23,17 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        // Set the content type of response to "text/html"
-        response.setContentType("text/html");
-
-        // Get the print writer object to write into the response
-        PrintWriter out = response.getWriter();
-
-        // Get the session object
-        HttpSession session = request.getSession();
-
-        // Get User entered details from the request using request parameter.
         String user = request.getParameter("username");
         String password = request.getParameter("password");
 
-        // Validate the password - If password is correct,
-        // set the user in this session
-        // and redirect to welcome page
-        if (password.equals("admin")) {
-            session.setAttribute("user", user);
-            response.sendRedirect("secured/profile.html?name=" + user);
+        if (password.equals("password") && user.equals("admin")) {
+            HttpSession session = request.getSession();
+            session.setAttribute("authenticated", true);
+            session.setAttribute("username", user);
+            response.sendRedirect("secured/profile.html");
         }
-        // If the password is wrong, display the error message on the login page.
         else {
-            RequestDispatcher rd = request.getRequestDispatcher("login.html");
-            out.println("<font color=red>Password is wrong.</font>");
-            rd.include(request, response);
+            response.sendRedirect("login.html?error=true");
         }
-        // Close the print writer object.
-        out.close();
     }
 }
