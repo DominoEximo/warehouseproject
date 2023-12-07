ae59be1767ca0bb961c9e386a9746c263a7f2ede
PappDávid <PappDavid32@gmail.com>
2023-12-07 13:47:47 +0100

    added documentation to service methods

--- warehouse-client-api/src/main/java/hu/neuron/mentoring/clientapi/service/CategoryService.java
+++ warehouse-client-api/src/main/java/hu/neuron/mentoring/clientapi/service/CategoryService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface CategoryService {
 
+
     Category findById(long id);
 
     Category findByName(String name);
--- warehouse-client-api/src/main/java/hu/neuron/mentoring/clientapi/service/ProductService.java
+++ warehouse-client-api/src/main/java/hu/neuron/mentoring/clientapi/service/ProductService.java
@@ -16,11 +16,36 @@ public interface ProductService extends Serializable {
 
     void deleteProduct(long id);
 
+    /**
+     * Return all the present categories from the database
+     * @return list of categories
+     */
     List<Category> getCategories();
+
+    /**
+     * Return all the present units from the database
+     * @return list of units
+     */
     List<Unit> getUnits();
 
+    /**
+     * returns a list of filtered and paginated products
+     * @param page page number
+     * @param length size of the page
+     * @param category category to filter by
+     * @return a list of products
+     */
     List<Product> getByCategoryPaginated(int page,int length,Category category);
+
+    /**
+     * returns a list of products by a given category
+     * @param category the filter
+     * @return a filterey list of products
+     */
     List<Product> getAllByCategory(Category category);
 
+    /**
+     * fills the database with predefined data
+     */
     void setUpMockedData();
 }
